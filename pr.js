6098152af4cdc485d247831cbf460f2c09aea30b
naghti <57380294+naghti@users.noreply.github.com>
2021-07-19 21:41:21 +0700

    Create pr.js

---
+++ pr.js
@@ -0,0 +1,1 @@
+function testWebP(b){var a=new Image();a.onload=a.onerror=function(){b(a.height==2)};a.src="data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA"}testWebP(function(a){if(a==true){document.querySelector("body").classList.add("webp")}else{document.querySelector("body").classList.add("no-webp")}});
